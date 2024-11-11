6a5bd868bb3e22ad5aa0e4c8136fab07e8df818c
Henry Agu <aguhenrychuks@gmail.com>
2024-11-12 00:31:27 +0100

    fix: deleted unwanted starter packs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,25 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import {Sorts_Mill_Goudy} from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
+
+const urwBold = localFont({
+  src: "./fonts/URWClassico-Bold.ttf",
+  variable: "--font-urw-bold",
   weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+})
+
+const urwRegular = localFont({
+  src: "./fonts/URWClassico-Regular.ttf",
+  variable: "--font-urw-regular",
+  weight: "100 400 900",
+})
+
+const sortsMillyGoudy = Sorts_Mill_Goudy({
+  subsets: ["latin"],
+  weight: ['400'],
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${sortsMillyGoudy.className} ${urwBold.variable} ${urwRegular.variable} antialiased`}
       >
         {children}
       </body>
