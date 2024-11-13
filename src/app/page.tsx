5a632aaea9dfb37cfae9481aef3b31de4f63d05c
Henry Agu <aguhenrychuks@gmail.com>
2024-11-13 21:04:41 +0100

    feat: quick dummy layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,10 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="container mx-auto">
-      <div className="flex flex-col gap-y-2 items-center justify-center py-14 pb-24">
-        <h1 className="uppercase font-vogue text-6xl xl:text-9xl font-black">oba nathan</h1>
+      <header className="flex flex-col gap-y-2 items-center justify-center py-14 pb-24">
+        <h1 className="uppercase font-vogue text-6xl xl:text-9xl font-black">
+          oba nathan
+        </h1>
         <div className="flex items-center gap-x-4">
           <Link href="">
             <Image
@@ -35,10 +37,80 @@ export default function Home() {
             />
           </Link>
         </div>
-      </div>
-      <div className="px-8">
-        image section
-      </div>
+      </header>
+      {/* Big screen version */}
+      <section className="hidden xl:flex flex-col gap-y-2 pb-24">
+        <div className="flex gap-2">
+          <div className="basis-[65%] flex items-center justify-center drop-shadow-md bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[650px] grayscale hover:grayscale-0 transition duration-300 ease-in-out">
+            <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+              Lifestyle
+            </p>
+          </div>
+          <div className="basis-[35%] flex items-center justify-center drop-shadow-md bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[650px] grayscale hover:grayscale-0 transition duration-300 ease-in-out">
+            <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+              Lifestyle
+            </p>
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <div className="basis-[35%] flex items-center justify-center drop-shadow-md bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[650px] grayscale hover:grayscale-0 transition duration-300 ease-in-out">
+            <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+              Lifestyle
+            </p>
+          </div>
+          <div className="basis-[65%] flex items-center justify-center drop-shadow-md bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[650px] grayscale hover:grayscale-0 transition duration-300 ease-in-out">
+            <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+              Lifestyle
+            </p>
+          </div>
+        </div>
+        <div className="flex gap-2">
+          <div className="basis-[65%] flex items-center justify-center drop-shadow-md bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[650px] grayscale hover:grayscale-0 transition duration-300 ease-in-out">
+            <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+              Lifestyle
+            </p>
+          </div>
+          <div className="basis-[35%] flex items-center justify-center drop-shadow-md bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[650px] grayscale hover:grayscale-0 transition duration-300 ease-in-out">
+            <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+              Lifestyle
+            </p>
+          </div>
+        </div>
+      </section>
+
+      {/* Small screen version */}
+      <section className="flex flex-col gap-y-2  xl:hidden w-full pb-24">
+        <div className="flex items-center justify-center drop-shadow-md grayscale bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[250px] w-full hover:grayscale-0 transition duration-300 ease-in-out">
+          <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+            Lifestyle
+          </p>
+        </div>
+        <div className="flex items-center justify-center drop-shadow-md grayscale bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[250px] w-full hover:grayscale-0 transition duration-300 ease-in-out">
+          <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+            Lifestyle
+          </p>
+        </div>
+        <div className="flex items-center justify-center drop-shadow-md grayscale bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[250px] w-full hover:grayscale-0 transition duration-300 ease-in-out">
+          <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+            Lifestyle
+          </p>
+        </div>
+        <div className="flex items-center justify-center drop-shadow-md grayscale bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[250px] w-full hover:grayscale-0 transition duration-300 ease-in-out">
+          <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+            Lifestyle
+          </p>
+        </div>
+        <div className="flex items-center justify-center drop-shadow-md grayscale bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[250px] w-full hover:grayscale-0 transition duration-300 ease-in-out">
+          <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+            Lifestyle
+          </p>
+        </div>
+        <div className="flex items-center justify-center drop-shadow-md grayscale bg-[url('/images/image.jpg')] bg-cover bg-center bg-no-repeat object-fill h-[250px] w-full hover:grayscale-0 transition duration-300 ease-in-out">
+          <p className="font-sorts uppercase text-xl font-semibold tracking-widest">
+            Lifestyle
+          </p>
+        </div>
+      </section>
     </main>
   );
 }
