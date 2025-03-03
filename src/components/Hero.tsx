cbdac3d1e8492dbd3ba1ddfc72451d4c4f805f66
Henry Agu <aguhenrychuks@gmail.com>
2025-03-03 15:42:19 +0100

    feat: mobile hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -9,6 +9,8 @@ interface HeroImages {
   height: number;
 }
 
+interface HeroSmallImages {}
+
 interface HeroProps {
   id: string;
 }
@@ -55,7 +57,7 @@ const heroImages: HeroImages[] = [
 const Hero: React.FC<HeroProps> = ({ id }) => {
   return (
     <main>
-      <div className="px-4 lg:px-8 xl:px-16 flex justify-between relative h-screen">
+      <div className="px-4 lg:px-8 xl:px-16 hidden lg:flex justify-between relative h-screen">
         {heroImages.map((image, index) => (
           <div
             key={index}
@@ -67,12 +69,52 @@ const Hero: React.FC<HeroProps> = ({ id }) => {
               alt="Image"
               width={image.width}
               height={image.height}
-              className={`w-[320px] h-[180px] aspect-[320/180] md:w-[${image.width}px] md:h-[${image.height}px] md:aspect-[${image.width}/${image.height}] grayscale-0 group-hover:grayscale transition duration-300 ease-in-out`}
+              className={`w-[${image.width}px] h-[${image.height}px] aspect-[${image.width}/${image.height}] grayscale-0 group-hover:grayscale transition duration-300 ease-in-out`}
             />
           </div>
         ))}
       </div>
-      <div className="relative mt-[-180px]">
+      <div className="flex flex-col lg:hidden h-screen px-4">
+        <div className="ml-14">
+          <Image
+            src="/images/1.jpg"
+            alt="image"
+            width={75}
+            height={120}
+            className="w-[75px] h-[120px]"
+          />
+        </div>
+        <div className="flex items-end justify-end mr-24">
+          <Image
+            src="/images/2.jpg"
+            alt="image"
+            width={98}
+            height={146}
+            className="w-[98px] h-[146px]"
+          />
+        </div>
+
+        <div className="flex gap-x-2 items-end justify-end mr-32 mt-20">
+          <Image
+            src="/images/3.jpg"
+            alt="image"
+            width={140}
+            height={210}
+            className="w-[140px] h-[210px]"
+          />
+        </div>
+
+        <div className="flex items-end justify-end mr-14 mt-20">
+          <Image
+            src="/images/4.jpg"
+            alt="image"
+            width={121}
+            height={181}
+            className="w-[121px] h-[181px]"
+          />
+        </div>
+      </div>
+      <div className="relative mt-[-150px] xl:mt-[-180px]">
         <h1
           className={`${gtSuperDisplay.className} text-4xl lg:text-[120px] xl:text-[160px] lg:leading-[120px] xl:leading-[150px] uppercase font-medium text-center`}
         >
