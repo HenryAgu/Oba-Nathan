2114f93b595e9bd0a846f38dc09f0b87454c75d8
Henry Agu <aguhenrychuks@gmail.com>
2025-10-30 10:48:56 +0100

    fix: vercel issue

--- src/components/icons/behance.tsx
+++ src/components/icons/behance.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BehanceIcon = ({ size = 24, color = "black", ...props }) => (
+const BehanceIcon = ({ size = 24, color = "black" }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width={size}
