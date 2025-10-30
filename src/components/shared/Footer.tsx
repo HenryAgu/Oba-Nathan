f9c09a71ab0a6d2a0a56a39f0b0314baf61e1b06
Henry Agu <aguhenrychuks@gmail.com>
2025-10-30 10:15:41 +0100

    refactor: footer svg changed

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,6 +1,39 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import InstagramIcon from "../icons/instagram";
+import XIcon from "../icons/x";
+import FacebookIcon from "../icons/facebook";
+import BehanceIcon from "../icons/behance";
+
+interface SocialMedia {
+  title: string;
+  icon: React.ReactNode;
+  path: string;
+}
+
+const socialMedia: SocialMedia[] = [
+  {
+    title: "Instagram",
+    icon: <InstagramIcon size={24} />,
+    path: "https://www.instagram.com/oba_nathan?igsh=MTVsNms3Ync0MTcw",
+  },
+  {
+    title: "x",
+    icon: <XIcon size={15} />,
+    path: "https://x.com/Oba__Nathan?t=GblSQE7bfeLs3f3i9mrLPQ&s=09",
+  },
+  {
+    title: "Facebook",
+    icon: <FacebookIcon size={24} />,
+    path: "https://www.facebook.com/reno.jnr.008?mibextid=ZbWKwL",
+  },
+  {
+    title: "Instagram",
+    icon: <BehanceIcon size={24} />,
+    path: "https://x.com/Oba__Nathan?t=GblSQE7bfeLs3f3i9mrLPQ&s=09",
+  },
+];
 
 const Footer = () => {
   return (
@@ -8,56 +41,20 @@ const Footer = () => {
       <div className="flex xl:flex-row justify-between border-[#989898] border-2 border-t-0 border-x-0 pb-3">
         <h3 className="text-3xl font-vogue font-black">OBA NATHAN</h3>
         <div className="flex items-center justify-start gap-x-3">
-          <Link
-            href="https://x.com/Oba__Nathan?t=GblSQE7bfeLs3f3i9mrLPQ&s=09"
-            target="_blank"
-          >
-            <Image
-              src="/images/x.svg"
-              alt="x-icon"
-              width={20}
-              height={20}
-              className="grayscale-0"
-            />
-          </Link>
-          <Link
-            href="https://www.facebook.com/reno.jnr.008?mibextid=ZbWKwL"
-            target="_blank"
-          >
-            <Image
-              src="/images/instagram.svg"
-              alt="x-icon"
-              width={20}
-              height={20}
-              className="grayscale-0"
-            />
-          </Link>
-
-          <Link
-            href="https://www.instagram.com/oba_nathan?igsh=MTVsNms3Ync0MTcw"
-            target="_blank"
-          >
-            <Image
-              src="/images/facebook.svg"
-              alt="x-icon"
-              width={20}
-              height={20}
-              className="grayscale-0"
-            />
-          </Link>
-          <Link href="" target="_blank">
-            <Image
-              src="/images/behance.svg"
-              alt="x-icon"
-              width={20}
-              height={20}
-              className="grayscale-0"
-            />
-          </Link>
+          {socialMedia.map((item) => (
+            <Link
+              href="https://x.com/Oba__Nathan?t=GblSQE7bfeLs3f3i9mrLPQ&s=09"
+              target="_blank"
+            >
+              {item.icon}
+            </Link>
+          ))}
         </div>
       </div>
       <div className="py-3 pb-10 md:pb-16 text-center xl:text-left">
-        <p className="text-sm font-[sorts]">Made by <b className="text-[#454545]">Henry Agu</b></p>
+        <p className="text-sm font-[sorts]">
+          Made by <b className="text-[#454545]">Henry Agu</b>
+        </p>
       </div>
     </div>
   );
