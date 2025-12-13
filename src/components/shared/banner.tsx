4bbbdce92787083ae432fda66cf73711c700a54c
Henry Agu <aguhenrychuks@gmail.com>
2025-12-13 23:43:59 +0100

    fix: vercel wahala

--- src/components/shared/banner.tsx
+++ src/components/shared/banner.tsx
@@ -9,7 +9,7 @@ const Banner = () => {
   </span>
 
   <Link href="tel:+2349091748665" className="text-[47px] lg:text-[144px] font-normal tracking-tight leading-none hover:text-gray-500 transition duration-300 ease-in-out mt-3 lg:mt-0">
-    Let's Talk
+    Let&apos;s Talk
   </Link>
 
   <span className="text-lg lg:text-[22px] font-normal leading-20 mt-3">
